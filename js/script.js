affe607b46b8162b666b1fb889b9535a26491e15
Melya <melenko55@gmail.com>
2021-07-20 01:14:19 +0300

    Second exercise

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let amountOfFilms = prompt("How many films have you watched?", "Number of films...");
+let amountOfFilms = +prompt("How many films have you watched?", "Number of films...");
 const personalMovieDB = {
     count :  amountOfFilms,
     movies : {},
@@ -9,12 +9,32 @@ const personalMovieDB = {
     privat : false
 };
 
-let film = prompt("Last watched film?", "");
-let rate = prompt("Personal assessment?", "");
-personalMovieDB.movies[film] = rate;
 
-film = prompt("Last watched film?", "");
-rate = prompt("Personal assessment?", "");
-personalMovieDB.movies[film] = rate;
+
+for (let i = 0;  i < 2; i++) {
+
+    let film = prompt("Last watched film?", "");
+    while (film == '' || film == null || film.length > 50) {
+        film = prompt("Last watched film?", "");
+    }
+
+    let rate = prompt("Personal assessment?", "");
+    while (rate == '' || rate == null || rate.length > 50) {
+        rate = prompt("Personal assessment?", "");
+    }
+
+    personalMovieDB.movies[film] = rate;
+}
+
+if (personalMovieDB.count < 10) {
+    alert("Too few films have been watched");
+} else if (personalMovieDB.count < 30) {
+    alert("Typical viewer");
+} else if (personalMovieDB.count >= 30) {
+    alert("You are moviegoer!");
+} else {
+    alert("Error!");
+}
+
 
 console.log(personalMovieDB.movies);
