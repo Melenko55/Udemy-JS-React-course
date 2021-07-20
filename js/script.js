280c5bea803f94009f3e8a6f347aab504c6d0216
Melya <melenko55@gmail.com>
2021-07-20 11:19:40 +0300

    Third commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 'use strict';
 
-let amountOfFilms = +prompt("How many films have you watched?", "Number of films...");
+let amountOfFilms;
+
+
+function start() {
+    amountOfFilms = +prompt("How many films have you watched?", "Number of films...");
+    while (amountOfFilms == null || amountOfFilms == '' || isNaN(amountOfFilms)) {
+        amountOfFilms = +prompt("How many films have you watched?", "Number of films...");
+    }
+}
+
+start();
+
 const personalMovieDB = {
     count :  amountOfFilms,
     movies : {},
@@ -9,32 +20,53 @@ const personalMovieDB = {
     privat : false
 };
 
+function rememberMyFilms() {
+    for (let i = 0;  i < 2; i++) {
 
-
-for (let i = 0;  i < 2; i++) {
-
-    let film = prompt("Last watched film?", "");
-    while (film == '' || film == null || film.length > 50) {
-        film = prompt("Last watched film?", "");
+        let film = prompt("Last watched film?", "");
+        while (film == '' || film == null || film.length > 50) {
+            film = prompt("Last watched film?", "");
+        }
+    
+        let rate = prompt("Personal assessment?", "");
+        while (rate == '' || rate == null || rate.length > 50) {
+            rate = prompt("Personal assessment?", "");
+        }
+    
+        personalMovieDB.movies[film] = rate;
     }
-
-    let rate = prompt("Personal assessment?", "");
-    while (rate == '' || rate == null || rate.length > 50) {
-        rate = prompt("Personal assessment?", "");
-    }
-
-    personalMovieDB.movies[film] = rate;
 }
 
-if (personalMovieDB.count < 10) {
-    alert("Too few films have been watched");
-} else if (personalMovieDB.count < 30) {
-    alert("Typical viewer");
-} else if (personalMovieDB.count >= 30) {
-    alert("You are moviegoer!");
-} else {
-    alert("Error!");
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert("Too few films have been watched");
+    } else if (personalMovieDB.count < 30) {
+        alert("Typical viewer");
+    } else if (personalMovieDB.count >= 30) {
+        alert("You are moviegoer!");
+    } else {
+        alert("Error!");
+    }
 }
 
+detectPersonalLevel();
 
-console.log(personalMovieDB.movies);
+
+function showMyDB() {
+    if (!personalMovieDB.privat) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        let genre = prompt(`Your favourite movie genre ${i}: `);
+        personalMovieDB.genres[i - 1] = genre;
+    }
+}
+
+writeYourGenres();
