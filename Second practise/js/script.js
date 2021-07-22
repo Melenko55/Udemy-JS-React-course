7bcd759b4291c4c20e78e07097f27a6a15e962fa
Melya <melenko55@gmail.com>
2021-07-22 15:53:47 +0300

    Actions with page

--- Second practise/js/script.js
+++ Second practise/js/script.js
@@ -1,8 +1,8 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+
 
 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
 Реализовать только при помощи JS
@@ -10,7 +10,7 @@
 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
 Отсортировать их по алфавиту 
 
-5) Добавить нумерацию выведенных фильмов */
+*/
 
 'use strict';
 
@@ -23,4 +23,39 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+
+const poster = document.querySelector(".promo__bg"),
+    movieList = document.querySelectorAll(".promo__interactive-list")[0];
+
+    
+//1) Удалить все рекламные блоки со страницы (правая часть сайта)
+document.querySelectorAll(".promo__adv img").forEach(element => {
+    element.remove();
+});
+
+
+//2) Изменить жанр фильма, поменять "комедия" на "драма"
+poster.querySelector(".promo__genre").textContent = "Драма";
+
+//3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+
+//4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+//Отсортировать их по алфавиту 
+//5) Добавить нумерацию выведенных фильмов 
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, index) => {
+    
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${index + 1} ${film}
+    <div class="delete"></div>
+    </li>`;
+});
+
+console.log(movieList);
+
 
