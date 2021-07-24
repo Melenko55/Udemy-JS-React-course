6b6af9675235f0b56d0345604734013270f3fcf7
Melya <melenko55@gmail.com>
2021-07-24 12:05:54 +0300

    Cards class added

--- FoodDisp/js/script.js
+++ FoodDisp/js/script.js
@@ -142,4 +142,60 @@ window.addEventListener("DOMContentLoaded", () => {
     } 
 
     const modalTimerId = setTimeout(showModal, 10000);
+
+
+    //Menu cards
+
+    const cardContainer = document.querySelector("[data-cards]");
+
+    console.log(cardContainer);
+    class Card {
+        constructor(src, alt, subtitle, description, price) {
+            this.src = src;
+            this.alt = alt;
+            this.subtitle = subtitle;
+            this.description = description;
+            this.price = this.changeToUAH(price);
+        }
+
+        changeToUAH(price, exchangeRate = 27) {
+            return exchangeRate * price;
+        }
+
+        createCard() {
+            const element = document.createElement('div');
+            element.classList.add("menu__item");
+            element.innerHTML = `
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.subtitle}</h3>
+            <div class="menu__item-descr">${this.description}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>`;
+            return element;
+        }
+
+        render(cardContainer) {
+            cardContainer.append(this.createCard());
+        }
+    }
+
+    const fitnessCard = new Card("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов.\
+         Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 15);
+
+    const premiumCard = new Card("img/tabs/elite.jpg", "elite", 'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд.\
+     Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 20);
+
+     const meatlessCard = new Card("img/tabs/post.jpg", "post", 'Меню "Постное"', 
+     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения,\
+      молоко из миндаля, овса, кокоса или гречки,\
+       правильное количество белков за счет тофу и импортных вегетарианских стейков.', 17);
+
+    fitnessCard.render(cardContainer);
+    premiumCard.render(cardContainer);
+    meatlessCard.render(cardContainer);
 });
