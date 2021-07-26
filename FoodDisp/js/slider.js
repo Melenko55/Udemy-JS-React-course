18edc8096c358a865b8a2728ef351e6b236a5163
Melya <melenko55@gmail.com>
2021-07-26 12:44:23 +0300

    Added dots to slider

--- FoodDisp/js/slider.js
+++ FoodDisp/js/slider.js
@@ -3,6 +3,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const prevArrow = document.querySelector(".offer__slider-prev"),
         nextArrow = document.querySelector(".offer__slider-next"),
         slides = document.querySelectorAll(".offer__slide"),
+        slider = document.querySelector(".offer__slider"),
         total = document.querySelector("#total"),
         current = document.querySelector("#current"),
         slidesWrapper = document.querySelector(".offer__slider-wrapper"),
@@ -30,6 +31,50 @@ window.addEventListener("DOMContentLoaded", () => {
         slide.style.width = width;
     });
 
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+        dots = [];
+
+
+    indicators.classList.add('carousel-indicators');
+    slider.append(indicators);
+
+    //Creating dots with indexes and pushing them into the array
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.classList.add("dot");
+        indicators.append(dot);   
+        dots.push(dot);
+    }
+    const moveSlide = () => slidesField.style.transform = `translateX(-${offset}px)`;
+
+    function showDots() {
+        dots.forEach(dot => {
+            dot.style.opacity = '.5';
+        });
+
+        dots[currentSlide - 1].style.opacity = 1;
+    }
+
+    function changeSlidesNumber() {
+        if (slides.length < 10) {
+            current.textContent = `0${currentSlide}`;
+        } else {
+            current.textContent = currentSlide;
+        }
+    }
+
+    function makeChanges() {
+        showDots();
+        changeSlidesNumber();
+        moveSlide();
+    }
+
     nextArrow.addEventListener('click', () => {
         if (offset == +width.slice(0, width.length - 2) * (slides.length - 1) ){
             offset = 0;
@@ -43,13 +88,7 @@ window.addEventListener("DOMContentLoaded", () => {
             currentSlide++;
         }
 
-        if (slides.length < 10) {
-            current.textContent = `0${currentSlide}`;
-        } else {
-            current.textContent = currentSlide;
-        }
-
-        slidesField.style.transform = `translateX(-${offset}px)`;
+       makeChanges();   
     });
 
     prevArrow.addEventListener('click', () => {
@@ -66,52 +105,18 @@ window.addEventListener("DOMContentLoaded", () => {
             currentSlide--;
         }
 
-        if (slides.length < 10) {
-            current.textContent = `0${currentSlide}`;
-        } else {
-            current.textContent = currentSlide;
-        }
+        makeChanges();
     });
 
-    /*showSlide(currentSlide);
+    dots.forEach( dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+            currentSlide = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
 
-    if (slides.length < 10) {
-        total.textContent = `0${slides.length}`;
-    } else {
-        total.textContent = slides.length;
-    }
-
-    function showSlide(index) {
-        
-        if (currentSlide > slides.length) {
-            currentSlide = 1;
-        }
-
-        if (currentSlide < 1) {
-            currentSlide = slides.length;
-        }
-
-        slides.forEach( (slide) => {
-            slide.style.display = "none";
+            makeChanges();
         });
+    });
 
-        slides[currentSlide - 1].style.display = "block";
-        if (currentSlide < 10) {
-            current.textContent = `0${currentSlide}`;
-        } else {
-            current.textContent = currentSlide;
-        }
-    }
-
-    function plusSlides(n) {
-        showSlide(currentSlide += n);
-    }
-
- 
-
-    prevArrow.addEventListener('click', () => plusSlides(-1));
-    nextArrow.addEventListener('click', () => plusSlides(1));
-*/
-
-
+    
 });
